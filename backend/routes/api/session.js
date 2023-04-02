836af91d266a78a0c396cf8bbed4d16e38f8d107
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-04-02 18:42:27 -0500

    fixed get reviews of current user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -32,10 +32,16 @@ router.post('/', validateLogin, async (req, res, next) => {
         return next(err);
       }
   
-      await setTokenCookie(res, user);
+      const token = await setTokenCookie(res, user);
   
       return res.json({
-        user: user
+        user: {
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          username: user.username
+        }
       });
     }
   );
