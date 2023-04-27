df7cc3bd5b974afc6d339d5e35cf315d01ec670d
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-04-27 17:26:42 -0500

    backend commit /sessions

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -25,11 +25,15 @@ router.post('/', validateLogin, async (req, res, next) => {
       const user = await User.login({ credential, password });
   
       if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = { credential: 'The provided credentials were invalid.' };
-        return next(err);
+        return res.status(401).json({
+          "message": "Invalid credentials",
+          "statusCode": 401
+        })
+        // const err = new Error('Login failed');
+        // err.status = 401;
+        // err.title = 'Login failed';
+        // err.errors = { credential: 'The provided credentials were invalid.' };
+        // return next(err);
       }
   
       const token = await setTokenCookie(res, user);
