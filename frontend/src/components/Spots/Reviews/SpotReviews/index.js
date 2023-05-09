1c77c850567dfa33a349223f43da2c8d4c9fa53f
Steven Sauseda <108492423+SSauseda@users.noreply.github.com>
2023-05-08 20:04:20 -0500

    able to populate reviews, button to create review, and button to submit review

---
+++ frontend/src/components/Spots/Reviews/SpotReviews/index.js
@@ -0,0 +1,141 @@
+import { spotDetailThunk } from "../../../../store/spots";
+import { getReviewsThunk } from "../../../../store/reviews";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { deleteReviewThunk } from "../../../../store/reviews";
+import CreateReviewModal from "../CreateReview/CreateReviewModal";
+import DeleteReviewModal from "../CreateReview/DeleteReviewModal";
+import CreateReviewForm from "../CreateReview";
+
+
+export default function SpotReviews ({ createNewReview }) {
+    const reviews = useSelector(state => state.review.allReviews)
+    console.log("TESTREVIEWSTESTREVIEWSTESTREVIEWSTESTREVIEWS" ,reviews)
+    const spot = useSelector(state => state.spot.spotDetails)
+    const sessionUser = useSelector(state => state.session.user)
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const [showCreateReviewModal, setShowCreateReviewModal] = useState(false)
+    const [deleteModalOpen, setDeleteModalOpen] = useState(false)
+    const [reviewToDelete, setReviewToDelete] = useState(null)
+    const [showOverlay, setShowOverlay] = useState(false)
+
+    // const { avgStarRating } = spotDetails
+
+    const createReviewClick = () => {
+        setShowCreateReviewModal(true)
+      }
+    
+      const handleDelete = reviewId => {
+        setReviewToDelete(reviewId)
+        setDeleteModalOpen(true)
+      }
+    
+      const handleDeleteConfirm = reviewId => {
+        if (reviewToDelete) {
+          dispatch(deleteReviewThunk(reviewToDelete))
+          setReviewToDelete(null)
+        }
+        setDeleteModalOpen(false)
+        history.push(`/`)
+      }
+    
+      const handleDeleteCancel = () => {
+        setReviewToDelete(null)
+        setDeleteModalOpen(false)
+      }
+      
+    
+      if (!reviews) return null;
+
+
+    return (
+        <div>
+        <h1>TEST REVIEW!</h1>
+          <div className='reviews-header'>
+            <h1 className='stars-rating'>
+              ★
+              <span className='avg-rating'>
+                {Number(spot.avgStarRating) ? Number(spot.avgStarRating).toFixed(1) : 'New'}
+              </span>
+              {reviews.length > 0 && <span className='dot'></span>}
+            </h1>
+            {reviews.length > 0 && (
+              <h1 className='num-reviews'>{spot.numReviews} Reviews </h1>
+            )}
+          </div>
+          {sessionUser &&
+            sessionUser.id !== spot.Owner.id &&
+            !reviews.some(review => review.userId === sessionUser.id) && (
+              <div>
+                <button className='post-review-btn' onClick={createReviewClick}>
+                  Post Your Review
+                </button>
+              </div>
+            )}
+          {reviews.length > 0 ? (
+            <ul className='reviews-container'>
+              {reviews.map(review => {
+                const date = new Date(review.updatedAt).toLocaleDateString(
+                  'en-US',
+                  {
+                    month: 'long',
+                    year: 'numeric'
+                  }
+                )
+    
+                return (
+                  <div className='review-container'>
+                    <div className='container' key={review.id}>
+                      <h3 className='review-user'>{review.User.firstName}</h3>
+                      <p className='review-date'>{date}</p>
+    
+                      <div className='desc'>{review.review}</div>
+                    </div>
+                    {sessionUser && sessionUser.id === review.userId && (
+                      <button
+                        className='delete-review-btn'
+                        onClick={() => handleDelete(review.id)}
+                      >
+                        Delete Review
+                      </button>
+                    )}
+                  </div>
+                )
+              })}
+            </ul>
+          ) : (
+            sessionUser &&
+            sessionUser.id !== spot.Owner.id && (
+              <button>Be the first to post a Review!</button>
+            )
+          )}
+          <CreateReviewModal
+            open={showCreateReviewModal}
+            onClose={() => setShowCreateReviewModal(false)}
+          >
+            <CreateReviewForm
+              spotId={spot.id}
+              createNewReview={createNewReview}
+              closeModal={() => setShowCreateReviewModal(false)}
+            />
+          </CreateReviewModal>
+    
+          {deleteModalOpen && (
+            <DeleteReviewModal
+              isOpen={deleteModalOpen}
+              onDelete={handleDeleteConfirm}
+              onCancel={handleDeleteCancel}
+              onClose={() => {
+                setDeleteModalOpen(false)
+                setShowOverlay(false)
+              }}
+            />
+          )}
+        </div>
+      )
+
+}
--- frontend/src/components/Spots/SpotDetails/index.js
+++ frontend/src/components/Spots/SpotDetails/index.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
+import { getReviewsThunk } from "../../../store/reviews";
 import './SpotDetails.css'
+import SpotReviews from "../Reviews/SpotReviews";
+import { createReviewThunk } from "../../../store/reviews";
 
 import { spotDetailThunk, getSpotsThunk} from "../../../store/spots";
 
@@ -11,10 +14,13 @@ export default function SpotDetails() {
     const { spotId } = useParams();
     // const history = useHistory();
     const [isLoaded, setIsLoaded] = useState(false);
+    const [review, setReview] = useState('')
+    const [stars, setStars] = useState('')
+
     const sessionUser = useSelector((state) => state.session.user);
 
     const spot = useSelector((state) => state.spot.spotDetails);
-    console.log("SPOTPSOTPSOT", spot)
+    // console.log("SPOTPSOTPSOT", spot)
 
     useEffect(() => {
         dispatch(spotDetailThunk(spotId))
@@ -22,8 +28,33 @@ export default function SpotDetails() {
     }, [dispatch, spotId])
 
     useEffect(() => {
+        dispatch(getReviewsThunk(spotId))
+        .then(() => setIsLoaded(true));
+    }, [dispatch, spotId])
+
+    useEffect(() => {
         dispatch(getSpotsThunk());
     }, [dispatch])
+
+    const createNewReview = async (e, review, stars) => {
+        e.preventDefault();
+
+        await dispatch(createNewReview({ review, stars }, spotId)).catch(
+            async response => {
+                const errors = []
+                const data = await response.json();
+
+                if (data && data.errors) {
+                 errors = data.errors
+                 console.log("errorserrors", errors)
+                }
+            }
+        )
+        await dispatch(spotDetailThunk(spotId)).then(() => 
+        dispatch(getReviewsThunk(spotId))
+        )
+        setIsLoaded(true)
+    }
 
     if (!spot) return null;
     if (!isLoaded) return null;
@@ -32,26 +63,112 @@ export default function SpotDetails() {
     // console.log("STPOSTPOIAMGEIAMGE",spotImg)
 
 
+
+
     return (
-        <div>
-         <div className="spotDetail-container">
-             <div className="spotName">
-                 <h1>{spot.name}</h1>
-             </div>
-             <div className="spotInfo">
-                 {/* <i class="fa-solid fa-star"></i>
-                    {Number(spot.avgStarRating) ? Number(spot.avgStarRating).toFixed(1) : 'No Reviews'}
-                         {" "}- {spot.numReviews} review(s) -  */}
-                         {spot.city}, {spot.state}, {spot.country}
-             </div>
-             <div className="spotImages">
-                <div className="primaryImg">
-                    <img src={spotImg[0].url} alt='primaryImage' className="primaryImage"/>
+    //     <div>
+    //      <div className="spotDetail-container">
+    //          <div className="spotName">
+    //              <h1>{spot.name}</h1>
+    //          </div>
+    //          <div className="spotInfo">
+    //              {/* <i class="fa-solid fa-star"></i>
+    //                 {Number(spot.avgStarRating) ? Number(spot.avgStarRating).toFixed(1) : 'No Reviews'}
+    //                      {" "}- {spot.numReviews} review(s) -  */}
+    //                      {spot.city}, {spot.state}, {spot.country}
+    //          </div>
+    //          <div className="spotImages">
+    //             <div className="primaryImg">
+    //                 <img src={spotImg[0].url} alt='primaryImage' className="primaryImage"/>
+    //             </div>
+    //          </div>
+    //      </div>
+    //      <h2>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2>
+    //      {spot.description}
+    //      </div>
+    <div className="spot-details-container">
+        <div className="spot-details">
+            <div className="spot-deatils-header">
+                <h1 className="spot-name">{spot.name}</h1>
+                <div className="spot-locaiton">
+                    {spot.city}, {spot.state}, {spot.country}
                 </div>
-             </div>
-                {spot.description}
-         </div>
-         </div>
+            </div>
+            <div className="grid-container">
+                <div className="spot-images">
+                    <img
+                    src={spotImg[0].url}
+                    key={spotImg[0].id}
+                    alt='primaryImage'
+                    className="primary-image"
+                    />
+                </div>
+                <div className="other-image-grid">
+                    Coming soon!
+                </div>
+            </div>
+            <h2 className="Hosted-text">
+                Hosted by {spot.Owner.firstName} {spot.Owner.lastName}
+            </h2>
+            <div className="description">
+                <div className="spot-description">
+                    {spot.description}
+                </div>
+                <div>
+                    Reservation box
+                    <div className="spot-price-box">
+                        <div className="spot-price">
+                            ${spot.price}/night
+                            <div className="stars-reviews-box">
+                                {Number(spot.avgStarRating) ? (
+                                    <>
+                                    <div className="spot-stars">
+                                        <p>★</p>
+                                        <div>{Number(spot.avgStarRating).toFixed(1)}</div>
+                                    </div>
+                                    </>
+                                ) : (
+                                    <>
+                                    <div className="spot-stars-new">
+                                        <p>★</p>
+                                        <div className="new-stars">New</div>
+                                    </div>
+                                    </>
+                                )}
+                                {spot.numReviews > 0 ? (
+                                    <p className="dot">·</p>
+                                ) : (
+                                    <p className="white-space"></p>
+                                )}
+                                <div className="num-reviews">
+                                    {spot.numReviews > 0 ? (
+                                        <div className="num-reviews">
+                                            {spot.numReviews === 1 ? '1 Review' : `${spot.numReviews} Reviews`}
+                                        </div>
+                                    ) : (
+                                        <br></br>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="spot-reserve">
+                            <button
+                            className="reserve-button"
+                            onClick={() => alert('Feature coming soon!')}
+                            >
+                                Reserve
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <SpotReviews createNewReview={createNewReview}/>
+            </div>
+        </div>
+
+    </div>
     )
 }
+
 
